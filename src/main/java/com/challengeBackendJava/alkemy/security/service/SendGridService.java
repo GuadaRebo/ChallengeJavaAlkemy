af40edcbf947a436ddf17d75a3baf6171283df4e
GuadaRebo <86989556+GuadaRebo@users.noreply.github.com>
2022-05-24 09:14:42 -0300

    sendgrid

--- src/main/java/com/challengeBackendJava/alkemy/security/service/SendGridService.java
+++ src/main/java/com/challengeBackendJava/alkemy/security/service/SendGridService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SendGridService {
-    SendGrid sg = new SendGrid("AlkemyKey");
+    SendGrid sg = new SendGrid("Alkemy");
 
     public void sendRequest(SendGridMail sendGridMail) throws IOException {
 
